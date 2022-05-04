cce9504f3c5d1d6dedc2925cb0c2a6e70e739aa2
Lucas Felipe <contatolucasfelipe.s@gmail.com>
2022-05-04 16:53:00 -0300

    feat: adiciona carousel na seção serviços

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,8 @@
 function onScroll(){
     if(scrollY > 0){
-        header.classList.add("scroll")
+        document.getElementById("header").classList.add("scroll")
     }else{
-        header.classList.remove("scroll")
+        document.getElementById("header").classList.remove("scroll")
     }
 }
 
@@ -12,4 +12,53 @@ function menuOpen(){
 
 function menuClose(){
     body.classList.remove("menu__expanded")
+}
+
+document.getElementById("radio1").addEventListener("click",function radio1(){
+    count=1;
+})
+
+document.getElementById("radio2").addEventListener("click",function radio2(){
+    count=2;
+})
+
+document.getElementById("radio3").addEventListener("click",function radio3(){
+    count=3;
+})
+
+document.getElementById("radio4").addEventListener("click",function radio4(){
+    count=4;
+})
+
+document.getElementById("radio5").addEventListener("click",function radio5(){
+    count=5;
+})
+
+document.getElementById("radio6").addEventListener("click",function radio6(){
+    count=6;
+})
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+var myTimer = setInterval(function(){
+    nextImage();
+}, 4000)
+
+function verificaClick(){
+    clearInterval(myTimer);
+    myTimer = setInterval(function(){
+        nextImage();
+    }, 4000);
+}
+
+function nextImage(){
+    count++;
+    if(count > 6){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+
 }
