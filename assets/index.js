523274301ddd65024343f89365b351bdbf1df3e8
Lucas Felipe <contatolucasfelipe.s@gmail.com>
2022-05-07 12:58:05 -0300

    feat: adicionar script de efeito active no menu do header

--- assets/index.js
+++ assets/index.js
@@ -1,70 +1,101 @@
 //SCROLL
-
-window.addEventListener("scroll", onScroll)
+window.addEventListener("scroll", onScroll);
 
 onScroll()
 function onScroll(){
     showNavOnScroll();
-    showBackToTopButtonOnScroll()
+    showBackToTopButtonOnScroll();
+
+    activeMenuAtCurrentSection(home);
+    activeMenuAtCurrentSection(servicos);
+    activeMenuAtCurrentSection(sobre);
+    activeMenuAtCurrentSection(depoimentos);
+    activeMenuAtCurrentSection(contato);
 }
 
 function showNavOnScroll(){
     if(scrollY > 0){
-        document.body.classList.add("scroll")
+        document.body.classList.add("scroll");
     }else{
-        document.body.classList.remove("scroll")
+        document.body.classList.remove("scroll");
     }
 }
 
 function showBackToTopButtonOnScroll(){
     if(scrollY > 250){
-        document.getElementById("backToTopButton").classList.add("show")
+        document.getElementById("backToTopButton").classList.add("show");
     }else{
-        document.getElementById("backToTopButton").classList.remove("show")
+        document.getElementById("backToTopButton").classList.remove("show");
+    }
+}
+
+function activeMenuAtCurrentSection(section){
+    const headerHeight = header.clientHeight
+    const targetLine = scrollY + innerHeight / 2;
+
+    // verificar se a seção passou da linha alvo
+    // quais dados vou precisar?
+    const sectionTop = headerHeight + section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+    // verficiar se a base está abaixo da linha alvo
+    //quais dados vou precisar?
+    const sectionEndsAt = headerHeight + sectionTop + sectionHeight;
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+    // limites da seção
+    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+    const sectionId = section.getAttribute('id');
+    const menuElement = document.querySelector(`.menu__header a[href*=${sectionId}]`);
+
+    menuElement.classList.remove('active');
+    if(sectionBoundaries){
+        menuElement.classList.add('active');
     }
 }
 
 //MENU EXPANDIDO
 function menuOpen(){
-    body.classList.add("menu__expanded")
+    body.classList.add("menu__expanded");
 }
 
 function menuClose(){
-    body.classList.remove("menu__expanded")
+    body.classList.remove("menu__expanded");
 }
 
-//CAROUSEL SERVIÇOS
-
-document.getElementById("servicos__menu").addEventListener("click",function radio1(){
-    count=1
+//CAROUSEL SERVIÇOS LOOP MOBILE
+document.getElementById("servicos__menu").addEventListener("click",function changeCountTo1(){
+    count=1;
     document.getElementById("radio1").checked = true;
     clearInterval(myTimer);
     myTimer = setInterval(function(){
         nextImage();
-    }, 6000);
+    }, 15000);
 })
 
-document.getElementById("radio1").addEventListener("click",function radio1(){
+document.getElementById("radio1").addEventListener("click",function changeCountToRadioNumber1(){
     count=1;
 })
 
-document.getElementById("radio2").addEventListener("click",function radio2(){
+document.getElementById("radio2").addEventListener("click",function changeCountToRadioNumber2(){
     count=2;
 })
 
-document.getElementById("radio3").addEventListener("click",function radio3(){
+document.getElementById("radio3").addEventListener("click",function changeCountToRadioNumber3(){
     count=3;
 })
 
-document.getElementById("radio4").addEventListener("click",function radio4(){
+document.getElementById("radio4").addEventListener("click",function changeCountToRadioNumber4(){
     count=4;
 })
 
-document.getElementById("radio5").addEventListener("click",function radio5(){
+document.getElementById("radio5").addEventListener("click",function changeCountToRadioNumber5(){
     count=5;
 })
 
-document.getElementById("radio6").addEventListener("click",function radio6(){
+document.getElementById("radio6").addEventListener("click",function changeCountToRadioNumber6(){
     count=6;
 })
 
@@ -95,7 +126,7 @@ function nextImage(){
 document.getElementById("radio"+count).checked = true;
 }
 
-//Scroll reveal
+//SCROLL REVEAL
 ScrollReveal({
     origin:"top",
     distance:"30px",
